4a0832e34811ad674a301ab2ff405eac2f009861
esteban <esteban.baron@etu.umontpellier.fr>
2021-12-03 00:26:39 +0100

    add footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,97 +10,71 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+
+const MainContainer = styled('div')(() => ({
+    display: "grid", 
+    maxWidth: "80em", 
+    marginRight: "auto", 
+    marginLeft: "auto",
+    gridTemplateRows: "5fr 1fr",
+    borderTop: "solid 1px rgba(189, 195, 199, 0.5)", 
 }));
+
+const ElemFooter = styled('div')(() => ({
+    display: "flex", 
+    flexFlow: "column", 
+    alignItems: "center",
+    justifyContent: "center", 
+}));
+
+const secondaryText = (name: string) => {
+    return (
+        <Typography color="text.secondary">
+            {name}
+        </Typography>
+    )
+};
 
 export default function footer() {
   return (
-    <div>
-        <Stack sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gridTemplateRows: "5fr 1fr"}}>
-            <Item>
-                <Box>
-                    <Card sx={{ minWidth: 275 }}>
-                        <CardContent>
-                            <Typography
-                                variant="h6"
-                                component="div"
-                            >
-                                Composition de l'equipe
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Box>
-            </Item>
-            <Item>
-                <Box>
-                    <Card sx={{ minWidth: 275 }}>
-                        <CardContent>
-                            <Typography
-                                variant="h6"
-                                component="div"
-                            >
-                                Liens utiles
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Box>
-            </Item>
-            <Item>
-                <Box>
-                    <Card sx={{ minWidth: 275 }}>
-                        <CardContent>
-                            <Typography
-                                variant="h6"
-                                component="div"
-                            >
-                                Contact
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                            <Typography color="text.secondary">
-                            well meaning and kindly.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Box>
-            </Item>
-        </Stack>
-        
-    </div>
+    <MainContainer>
+        <Box sx={{ display: "grid", gridTemplateColumns: "1fr 2fr 1fr" }}>
+            <ElemFooter>
+                <Typography
+                    variant="h6"
+                    component="div"
+                >
+                    Nom de l'equipe
+                </Typography>
+                {secondaryText("Fix Typo de Polytech Montpellier DO")}
+            </ElemFooter>
+            <ElemFooter>
+                <Typography
+                    variant="h6"
+                    component="div"
+                >
+                    Liens utiles
+                </Typography>
+                {secondaryText("Nils Ponsard")}
+                {secondaryText("Maxime Foucher")}
+            </ElemFooter>
+            <ElemFooter>
+                <Typography
+                    variant="h6"
+                    component="div"
+                >
+                    Contact
+                </Typography>
+                {secondaryText("nils.ponsard")}
+                {secondaryText("Maxime Foucher")}
+                {secondaryText("Mathieux Soysal")}
+            </ElemFooter>
+        </Box>
+        <Box sx= {{ display: "flex", flexFlow: "row", justifyContent: "center", alignItems: "center" }}>
+            <Typography>
+                ©2021 Copyright
+            </Typography>
+        </Box>
+    </MainContainer>
   );
 }
