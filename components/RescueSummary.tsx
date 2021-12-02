0383cc80f03a66c6d7985acf1359a6396d57d73b
esteban <esteban.baron@etu.umontpellier.fr>
2021-12-03 00:49:00 +0100

    add component RescueSummary

---
+++ components/RescueSummary.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function RescueSummary({ image, name, shortDescription }: { image: string; name: string; shortDescription: string; }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="image sauvetage"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {shortDescription}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
