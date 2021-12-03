1c75aaae7f1ecc23800477c9fd1967575f00de0d
Nils Ponsard <nilsponsard@gmail.com>
2021-12-03 07:15:51 +0100

    filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,20 @@ const Home: NextPage = () => {
     }
   }, []);
 
-  const { data, error } = useSWR('https://api.ndl.iverly.net/sauvetage', fetcher) as {
+
+
+
+
+
+
+  const Filter = (name: mode, selected: boolean) => {
+    return (
+      <Button onClick={() => { setFilter(name); }} sx={{ margin: "1em" }} variant={selected ? "contained" : "outlined"} >{name}</Button>
+    );
+  };
+
+  const [filter, setFilter] = useState(mode.Sauvetage);
+  const { data: dataSauvetage, error } = useSWR('https://api.ndl.iverly.net/sauvetage', fetcher) as {
     data: Array<{
       dateSauvetage: string,
       descriptionSauvetage: string,
@@ -64,18 +77,44 @@ const Home: NextPage = () => {
     }>, error: Error;
   };
 
-  const Filter = (name: mode, selected: boolean) => {
-    return (
-      <Button onClick={() => { setFilter(name); }} sx={{ margin: "1em" }} variant={selected ? "contained" : "outlined"} >{name}</Button>
-    );
+
+  const { data: dataBateau, error: errb } = useSWR('https://api.ndl.iverly.net/bateau', fetcher) as {
+    data: Array<{
+      dateNauffrage: string,
+      id: 0,
+      nom: string,
+      photo: string,
+      type: string;
+    }>, error: Error;
   };
 
-  const [filter, setFilter] = useState(mode.Sauvetage);
 
+  const { data: dataNaufragee, error: errn } = useSWR('https://api.ndl.iverly.net/naufragee', fetcher) as {
+    data: Array<{
+      dateNaissance: string,
+      id: 0,
+      nom: string,
+      prenom: string,
+      photo: string,
+      profession: string;
+    }>, error: Error;
+  };
+
+  const { data: dataSauver, error: errs } = useSWR('https://api.ndl.iverly.net/naufragee', fetcher) as {
+    data: Array<{
+      personne: {
+        nom: string,
+        prenom: string,
+        photo: string,
+        id: number,
+        description: string,
+      };
+    }>, error: Error;
+  };
   if (error) {
     return <div>Erreur</div>;
   }
-  if (!data) {
+  if (!dataSauvetage) {
     return <CircularProgress />;
   }
 
@@ -128,15 +167,47 @@ const Home: NextPage = () => {
           alignSelf: "center",
           justifySelf: "center"
         }}>
-          {
-            data.map((value, index) => {
-              return (
+          {() => {
+            switch (filter) {
+              case 'Sauvetage':
+                return dataSauvetage.map((value, index) => {
+                  return (
 
-                <Box sx={{ marginTop: "3em" }} key={index}>
-                  <SearchResult image={""} name={value.titre} shortDescription={value.descriptionSauvetage} type={filter} />
-                </Box>
-              );
-            })
+                    <Box sx={{ marginTop: "3em" }} key={index}>
+                      <SearchResult image={""} name={value.titre} shortDescription={value.descriptionSauvetage} type={filter} />
+                    </Box>
+                  );
+                });
+              case 'Sauveteur':
+                return dataSauver.map((value, index) => {
+                  return (
+
+                    <Box sx={{ marginTop: "3em" }} key={index}>
+                      <SearchResult image={""} name={value.personne.nom} shortDescription={value.personne.description} type={filter} />
+                    </Box>
+                  );
+                });
+              case 'Bateau':
+                return dataBateau.map((value, index) => {
+                  return (
+
+                    <Box sx={{ marginTop: "3em" }} key={index}>
+                      <SearchResult image={""} name={value.nom} shortDescription={value.type} type={filter} />
+                    </Box>
+                  );
+                });
+              case 'Naufragé':
+                return dataNaufragee.map((value, index) => {
+                  return (
+
+                    <Box sx={{ marginTop: "3em" }} key={index}>
+                      <SearchResult image={""} name={value.nom} shortDescription={value.profession} type={filter} />
+                    </Box>
+                  );
+                });
+            }
+
+          }
           }
         </Box>
       </MainContainer>
